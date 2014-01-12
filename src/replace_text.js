801ff8e11b5d2fbf28ffeeb290a112280d9f3dbd
tristanfisher <code@tristanfisher.com>
2014-01-12 16:57:59 -0500

    first pass

---
+++ src/replace_text.js
@@ -0,0 +1,43 @@
+traverseDOM(document.body);
+
+function traverseDOM(node)
+{
+
+    var child, sibling;
+
+    // with a head tilt to goo.gl/s4D2JW
+    switch (node.nodeType)
+    {
+        //skip these, we're not restructuring:
+        case 1: // element
+        case 9: // document
+        case 11: // document fragmment
+            child = node.firstChild;
+            while (child)
+            {
+                sibling = child.nextSibling;
+                traverse_dom(child);
+                child = sibling; //they grow up so fast;
+            }
+            break;
+        case 3: //text!
+            processRegex(node);
+            break;
+        default: //skip types we don't explicitly handle
+            break;
+    }
+} //traverse_dom()
+
+function processRegex(textNodeType)
+{
+    //use vars so we can expand to a list of similar terms later
+    var textNodeContent = textNodeType.nodeValue;
+    var termOld = 'bitcoin';
+    var termNew = 'dunning-kruggerrand';
+
+    // '\b' : word boundary; '(?i)(term)' any case
+    // i basically want replace(/\bbitcoin\b/g, meant), but for now:
+    var regExp = new RegExp(termOld, 'ig');
+    textNodeContent = textNodeContent.replace(regExp, termNew); //replace term
+    textNodeType.nodeValue = textNodeContent; //and put it on the page
+}
